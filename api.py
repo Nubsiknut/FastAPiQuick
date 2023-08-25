39356504c03d8c6a9c274db76811a75f52552dd4
Nubsiknut <haiko.didzoleit@gmail.com>
2023-08-25 12:55:32 +0200

    abasic works

---
+++ api.py
@@ -0,0 +1,65 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+app = FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",  # Add your frontend's origin(s) here
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# SQLite Database Setup
+DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+
+# Create the database tables
+Base.metadata.create_all(bind=engine)
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+
+class UserResponse(UserCreate):
+    id: int
+
+@app.post("/users/", response_model=UserResponse)
+def create_user(user: UserCreate):
+    db = SessionLocal()
+    db_user = User(**user.dict())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    db.close()
+    return db_user
+
+@app.get("/users/{user_id}", response_model=UserResponse)
+def get_user(user_id: int):
+    db = SessionLocal()
+    user = db.query(User).filter(User.id == user_id).first()
+    db.close()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
